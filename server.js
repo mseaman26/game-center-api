77d85880824f4c610de06e4fad5dfab6d65b4e98
michael seaman <mseaman26@gmail.com>
2023-06-22 00:18:51 -0700

    changed server back to old version

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const https = require('https');
-const fs = require('fs');
 const path = require('path');
 require('dotenv').config({path: '/home/ec2-user/game-center-api/.bashrc'})
 //require('dotenv').config()
@@ -11,17 +9,12 @@ require('dotenv').config({path: '/home/ec2-user/game-center-api/.bashrc'})
 const routes = require('./routes');
 
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'SSL', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'SSL', 'certificate.pem'))
-};
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
@@ -36,11 +29,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gameCente
   useUnifiedTopology: true,
 })
   .then(() => {
-    // Create an HTTPS server using the SSL options
-    const server = https.createServer(sslOptions, app);
-
-    server.listen(PORT, () => {
-      console.log(`HTTPS server running on port ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Now listening on port ${PORT}`);
     });
   })
   .catch((error) => {
